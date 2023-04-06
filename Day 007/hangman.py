17d7a1e0cb3a7044d98ea5ef4343ea2ce1874e04
Jere <jeremias.arnold15@gmail.com>
2023-04-06 00:37:20 -0600

    Hangman User Experience Tweeks

--- Day 007/hangman.py
+++ Day 007/hangman.py
@@ -1,15 +1,16 @@
 import random
+import os
 import art
+import words
 
 #Select a word
-word_list = ['ballon', 'pillow', 'banana', 'motorcycle', 'cheesecake', 'sleep', 'python']
-random.shuffle(word_list)
-chosen_word = word_list.pop().lower()
+chosen_word = random.choice(words.word_list)
 
 #Defining player variables
 player_guesses = ''
 valid_inputs = 'abcdefghijklmnopqrstuvwxyz'
 lives = 6
+game_on = True
 
 #Take player input and validate
 def request_guess():
@@ -24,7 +25,8 @@ def request_guess():
     else:
         player_guesses += player_input
         return player_input
-    
+
+#Create initial word display    
 def create_word_display(word):
     word_display = []
     for letter in word:
@@ -32,9 +34,9 @@ def create_word_display(word):
     print(' '.join(word_display))
     return word_display
 
+#Compare guess to chosen word and update game
 def update_game(word, guess, word_display):
     global lives
-    
     matched = False
     for position in range(len(word)):
         if guess == word[position]:
@@ -42,31 +44,32 @@ def update_game(word, guess, word_display):
             matched = True   
     if not matched:
         lives -= 1
-    display(word_display,lives)
+    display(word_display, lives, matched)
 
-def display(word_display, lives):
+#Print update to console
+def display(word_display, lives, matched):
+    os.system('cls')
     print(' '.join(word_display))
     print(art.stages[lives])
+    if not matched:
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
+    print('')
 
+#Check if game is over. Win or lose states
 def check_game_state(lives, word_display, chosen_word):
     if lives == 0:
         print(f'Word was {chosen_word}')
-        print('Game Lost')
+        print('Game Lost..')
         return False
     if '_' not in word_display:
-        print('Game Won')
+        print('Game Won!!')
         return False
     else:
         return True
 
-    
-### For testing only ###
-# print(f'pssst, the solution is {chosen_word}')
-### For testing only ###
-
 word_display = create_word_display(chosen_word)
 
-game_on = True
+print(art.logo)
 
 while game_on:
     guess = request_guess()
