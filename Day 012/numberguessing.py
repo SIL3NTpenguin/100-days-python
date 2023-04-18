0fabe8226e00bcfb0753b8d70797430b20c9eb19
Jere <jeremias.arnold15@gmail.com>
2023-04-17 23:42:42 -0600

    styling changes

--- Day 012/numberguessing.py
+++ Day 012/numberguessing.py
@@ -2,24 +2,30 @@ import random
 import os
 import art
 
+
 def pick_num():
     print("I'm thinking of a number between 1 and 100.")
-    return random.randint(1,100)
+    return random.randint(1, 100)
+
 
 def set_difficulty():
-    _attempts = {'easy': 10, 'hard': 5}
+    _attempts = {"easy": 10, "hard": 5}
     _choice = input("Choose a difficulty. type 'easy' or 'hard': ")
-    if _choice.lower() not in ('easy','hard'):
+    if _choice.lower() not in ("easy", "hard"):
         print("Only acceptable inputs are 'easy' or 'hard'. Please try again.")
         return set_difficulty()
     else:
         return _attempts[_choice.lower()]
 
+
 def win(answer):
-    print(f'You got it! The answer was {answer}')
+    print(f"You got it! The answer was {answer}")
+
 
 def lose(answer):
-    print(f"You've run out of guesses, you lose.\n The correct answer was {answer}")
+    print("You've run out of guesses, you lose.")
+    print(f"\n The correct answer was {answer}")
+
 
 def guess_num(target_num, attempts):
     print(f"You have {attempts} attempts remaining to guess the number.")
@@ -36,13 +42,15 @@ def guess_num(target_num, attempts):
         print("Too low.")
     return _guess, attempts
 
+
 def clear():
     # for windows
-    if os.name == 'nt':
-        _ = os.system('cls')
+    if os.name == "nt":
+        _ = os.system("cls")
     # for mac and linux(here, os.name is 'posix')
     else:
-        _ = os.system('clear')
+        _ = os.system("clear")
+
 
 def game():
     print(art.logo)
@@ -53,12 +61,13 @@ def game():
     while guess != answer and attempts != 0:
         guess, attempts = guess_num(answer, attempts)
     replay = input("Do you want to play again? 'y' or 'n': ")
-    if replay == 'y':
+    if replay == "y":
         clear()
         game()
     else:
         exit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     clear()
     game()
--- Day 014/higherlower.py
+++ Day 014/higherlower.py
@@ -1,32 +1,45 @@
+"""
+Main module for Higher Lower game.
+"""
+
+import os
+from random import randint
+
 import art
 import game_data
-from random import randint
-import os
+
 
 def clear():
+    """
+    Clears console
+    """
     # for windows
-    if os.name == 'nt':
-        _ = os.system('cls')
+    if os.name == "nt":
+        _ = os.system("cls")
     # for mac and linux(here, os.name is 'posix')
     else:
-        _ = os.system('clear')
+        _ = os.system("clear")
 
-def celebrity_selection(pre_sel=''):
-    choice = randint(0,len(game_data.data)-1)
+
+def celebrity_selection(pre_sel=""):
+    choice = randint(0, len(game_data.data) - 1)
     choice = game_data.data[choice]
-    if choice['name'] == pre_sel:
-        return(celebrity_selection(pre_sel))
+    if choice["name"] == pre_sel:
+        return celebrity_selection(pre_sel)
     else:
         return choice
-    
-def celeb_details(celeb: dict):
-    return f"{celeb['name']}, a {celeb['description']}, from {celeb['country']}"
 
-def new_round(last_celeb = None, score = None):
-    if last_celeb == None:
+
+def celeb_details(celeb: dict):
+    c = celeb
+    return f"{c['name']}, a {c['desc']}, from {c['country']}"
+
+
+def new_round(last_celeb=None, score=None):
+    if last_celeb is None:
         last_celeb = celebrity_selection()
     a = last_celeb
-    b = celebrity_selection(a['name'])
+    b = celebrity_selection(a["name"])
     clear()
     print(art.logo)
     if score != 0:
@@ -36,17 +49,19 @@ def new_round(last_celeb = None, score = None):
     print(f"Against B: {celeb_details(b)}")
     choice = input("Who has more followers? Type 'A' or 'B': ")
     new_score = 0
-    if choice.lower() == 'a' and a['follower_count'] > b['follower_count']:
+    if choice.lower() == "a" and a["follower_count"] > b["follower_count"]:
         new_score += 1
-    elif choice.lower() == 'b' and a['follower_count'] < b['follower_count']:
+    elif choice.lower() == "b" and a["follower_count"] < b["follower_count"]:
         new_score += 1
     return new_score, b
+
 
 def lose(score):
     clear()
     print(art.logo)
     print(f"Sorry, that's wrong. Final score: {score}")
     return False
+
 
 def higherlower():
     last_celeb = None
@@ -59,6 +74,7 @@ def higherlower():
         else:
             game_on = lose(score)
         print(score)
+
 
 if __name__ == "__main__":
     higherlower()
